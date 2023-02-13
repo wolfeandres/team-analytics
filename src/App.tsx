ce1c1b43b2fd99e3ccc624ec138e88ff0bbe42ee
Andres Wolfe <wolfe.andres@ufl.edu>
2023-02-13 15:35:30 -0500

    Small formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { LineChart, Line, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, Legend, BarChart, Bar } from 'recharts';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
 
 const data = [
-  {
-    month: 'January',
-    Joy: 4000,
-    Alex: 2400,
-  },
-  {
-    month: 'Febuary',
-    Joy: 3000,
-    Alex: 1398,
-  },
-  {
-    month: 'March',
-    Joy: 2000,
-    Alex: 9800,
-  },
-  {
-    month: 'April',
-    Joy: 2780,
-    Alex: 3908,
-  },
-  {
-    month: 'May',
-    Joy: 1890,
-    Alex: 4800,
-  },
-  {
-    month: 'June',
-    Joy: 2390,
-    Alex: 3800,
-  },
-  {
-    month: 'July',
-    Joy: 3490,
-    Alex: 4300,
-  }
+  { month: 'January', Joy: 4000, Alex: 2400,},
+  { month: 'Febuary', Joy: 3000, Alex: 1398,},
+  { month: 'March', Joy: 2000, Alex: 9800,},
+  { month: 'April', Joy: 2780, Alex: 3908,},
+  { month: 'May', Joy: 1890, Alex: 4800,},
+  { month: 'June', Joy: 2390, Alex: 3800,},
+  { month: 'July', Joy: 3490, Alex: 4300,}
 ];
 
 const renderLine = (
@@ -47,7 +21,7 @@ const renderLine = (
     height={300}
     data={data}
     margin={{
-      top: 5,
+      top: 20,
       right: 30,
       left: 20,
       bottom: 5,
@@ -61,65 +35,40 @@ const renderLine = (
     <Line type="monotone" dataKey="Joy" stroke="#8884d8" activeDot={{r:8}} />
     <Line type="monotone" dataKey="Alex" stroke="#82ca9d" />
   </LineChart>
-)
+);
+
+const renderBar = (
+  <BarChart
+    width={500}
+    height={300}
+    data={data}
+    margin={{
+      top: 20,
+      right: 30,
+      left: 20,
+      bottom: 5,
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="month" />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="Joy" fill="#8884d8" />
+    <Bar dataKey="Alex" fill="#82ca9d" />
+  </BarChart>
+);
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div style={{float:"left"}}>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Joy" stroke="#8884d8" activeDot={{r:8}} />
-          <Line type="monotone" dataKey="Alex" stroke="#82ca9d" />
-        </LineChart>
-      </div>
-      <div style={{overflow:"auto"}}>
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="Joy" fill="#8884d8" />
-          <Bar dataKey="Alex" fill="#82ca9d" />
-          </BarChart>
+    <div>
+      <AppBar position="static">
+        <Typography variant="h4" component="div" sx={{my:1, mx:1}}>
+          Online Dashboard
+        </Typography>
+      </AppBar>
+      <div className="Charts">
+        {renderLine}
+        {renderBar}
       </div>
     </div>
   );
