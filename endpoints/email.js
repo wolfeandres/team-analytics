e85b09a8f3ab00295be5d4294069cebbb78eab03
Luke <58048740+Shinigami@users.noreply.github.com>
2023-04-25 09:13:56 -0400

    Update email.js

--- endpoints/email.js
+++ endpoints/email.js
@@ -3,28 +3,36 @@ var moment = require('moment');
 
 exports = async function({ query, headers, body}, response) {
 
+    // Make sure that the Fit File is being sent as bytes.
     if (headers["Content-Type"][0] != "application/octet-stream") {
       return "Error: body must be of Content-Type: application/octet-stream.";
     }
     
+    // Get the Mail Header
     const mail = headers["Mail"];
     
+    // If this is null or the header doesn't have any data
     if (mail == null || mail.length == 0) {
       return "Error: you must provide an email in the Mail header.";
     }
     
+    // Get the email address
     const email = mail[0];
     
+    // If the email address is null, empty, not a string, or doesn't have an @, raise an error.
     if (email == null || email == "" || typeof email != "string" || email.indexOf("@") == -1) {
       return "Error: invalid email: " + email;
     }
     
+    // Get the filename header
     const fileName = headers["Filename"];
     
+    // If the header is null or has no data, throw an error.
     if (fileName == null || fileName.length == 0) {
       return "Error: you must provide a filename in the fileName header.";
     }
     
+    // Get the filename and make sure it's a valid string.
     var name = fileName[0];
     if (name == null || name == "" || typeof name != "string") {
       return "Error: invalid file name: " + name;
@@ -32,10 +40,12 @@ exports = async function({ query, headers, body}, response) {
 
     const reqBody = body;
 
+    // Check if there are bytes in the body, and the body isn't empty.
     if (reqBody == null || reqBody.text() == null || reqBody.text() == "") {
       return "Error: you must provide a body.";
     }
  
+    // Send the email, where the user is the created email and the password is to log in to the user.
     let transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 465,
@@ -46,11 +56,13 @@ exports = async function({ query, headers, body}, response) {
       }
     });
   
+    // Create a date to send in the email.
     const m = moment();
       
     const date = m.format("YYYY-MM-DD");
     const time = m.format("HH:mm:ss");
       
+    // Email formatting.
     const mailOptions = {
       from: 'partnerfitnessapp@gmail.com',
       to: email,
@@ -64,6 +76,7 @@ exports = async function({ query, headers, body}, response) {
         ]
     };
 
+  // Send the email
   return transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
