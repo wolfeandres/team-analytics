cd8ce95acc1e3dce93a50823537d3c10c6ace01f
Luke <58048740+Shinigami@users.noreply.github.com>
2023-04-25 09:14:29 -0400

    Update getAllEntries.js

--- endpoints/getAllEntries.js
+++ endpoints/getAllEntries.js
@@ -1,4 +1,6 @@
 exports = function({ query, headers, body}, response) {
+    // Find returns all documents without a query. 
     const doc = context.services.get("mongodb-atlas").db("FitnessLog").collection("Test").find();
+    // return the list of documents.
     return doc;
 };
